1bdaad38e403a8ee32ed6ba959483a62d08beaf2
Javier Garcia <93437997+IamYipi@users.noreply.github.com>
2022-03-14 21:42:32 +0100

    Update busca_cadena.c

--- busca_cadena.c
+++ busca_cadena.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <mpi.h>
+#define PESO_COMPROBAR  5000000
+#define PESO_GENERAR    10000000
+
+void fuerza_espera(unsigned long);
 
 int main(int argc, char ** argv){
-
 	if(argc != 2){
 		fprintf(stdout, "Error: Numero invalido de argumentos\n");
+		fprintf(stdout, "Usar: mpirun -np X [--oversubscribe] %s numero\n", argv[0]);
 		return 1;
 	}
-	
-	int myrank, nprocs;
+
+	int id, nprocs;
 
 	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-	printf("Hello from processor %d of %d\n",myrank, nprocs);
+	if(nprocs < 3){
+		fprintf(stdout,"Error: Numero invalido de procesos\n");
+		fprintf(stdout,"Finalizando programa...\n");
+		MPI_Finalize();
+		return 2;
+	}
+
+	printf("Hello from processor %d of %d\n",id, nprocs);
+
+	/*Tareas a realizar por el Proceso 0*/
+	
+	if(id == 0){
+
+
+		
+	}
 
 	MPI_Finalize();
-	return 0;
-	
-	
+	return 0;	
+}
+
+
+void fuerza_espera(unsigned long peso)
+{
+  for (unsigned long i=1; i<1*peso; i++) sqrt(i);
 }
