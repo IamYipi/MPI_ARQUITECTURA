b95988d13fd8d000195128baeafbc53a52887518
R4ykoo <germanfrancestostado@gmail.com>
2022-03-15 20:12:43 +0100

    Esqueleto principal del proceso E/S, queda comunicarse con el resto, estoy en ello.
    Comentarios para ayudar a la compilacion y ejecucion del codigo y su claridad

--- busca_cadena.c
+++ busca_cadena.c
@@ -12,7 +12,15 @@
 void fuerza_espera(unsigned long);
 
 int main(int argc, char ** argv){
-
+/*
+ejemplo compilacion: 	mpicc busca_cadena.c -o nombre_ejecutable -lm
+ejemplo ejecucion:   	mpirun -np 7 --oversubscribe nombre_ejecutable 2 0
+	 
+	 argumentos: 
+	 					argv[0] = nombre_ejecutable
+						argv[1] = numeroComprobadores
+						argv[2] = 0 si no hay pistas, 1 si hay pistas
+*/
 	if(argc != 2){
 		fprintf(stdout, "Error: Numero invalido de argumentos\n");
 		fprintf(stdout, "Usar: mpirun -np X [--oversubscribe] %s numero\n", argv[0]);
@@ -33,6 +41,7 @@ int main(int argc, char ** argv){
 		MPI_Finalize();
 		return 2;
 	}
+	
 	/*Tareas a realizar por el Proceso 0*/
 	
 	if(id == 0){
@@ -43,11 +52,43 @@ int main(int argc, char ** argv){
 		
 		printf("La longitud de la palabra es: %ld y hay %d comprobadores\n",
 							strlen(palabra), atoi(argv[1]));
+		
+		int numComp = atoi(argv[1]);
+		int numGen = nprocs - (numComp + 1); // +1 pq seria E/S 
+		
+		// Sabe cuantos Comprobadores existe, y si es posible seguir adelante con el nº de procs
+		if(numGen < 1){
+			fprintf(stdout,"Error: no hay ningun Generador\n");
+			fprintf(stdout,"Aumente el numero de procesos, o disminuya el numero de Comprobadores\n");
+			fprintf(stdout,"Finalizando programa...\n");
+			MPI_Finalize();
+			return 3;
+		}
+		
+		// Dependiendo del nprocs y del nºComprobadores, habrá un nºGeneradores
+		//   -> Indica a cada proceso su rol, si es Generador, le dice cual es su Comprobador
+		
+		
+		// Sabra si se usan pistas o no segun argv[2]
+		
+		// Envía tipo de proceso
+		
+		// Envía longitud
 		int i;
 		i= strlen(palabra);
 		MPI_Bcast( &i, 1, MPI_INT, 0, MPI_COMM_WORLD ); // Envía mensaje con la longitud a todos
+		// Envía la palabra a los Comprobadores
 		
 		
+		/* Espera a recibir de los Generadores
+		    -> Cadena con los caracteres ya encontrados
+				-> Si hay pistas, se distribuyen al resto de Generadores
+			-> Palabra ya encontrada
+		*/
+		
+		// Si palabra está encontrada manda terminar a Comprobadores y Generadores y recibe estadisticas
+		
+		MPI_Finalize();
 	}else{
 		int longitud;
 		
